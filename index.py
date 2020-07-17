27268fc274aad4919da87494e2179cd7fc616edf
Alfonso <alfonso@MacBook-Air-de-Alfonso.local>
2020-07-17 17:29:56 +0200

    fixing photos link

--- index.py
+++ index.py
@@ -4,15 +4,15 @@ from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 ###
 import time
+## to download the images
 import urllib.request
+## for easier scraping
 import bs4
-# driver = webdriver.Chrome('./chromedriver_2') 
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver.get("https://facebook.com/")
+
 username=input("Introduce your username:  ")
 password=input("Introduce your password:  ")
 
-
+## we set the chrome options to avoid notifications
 chrome_options = webdriver.ChromeOptions()
 
 prefs = {"profile.default_content_setting_values.notifications" : 2}
@@ -32,20 +32,27 @@ def check_Loaded(search,waiting):
             print("logged in!")
             break
 
+def login():
+    check_Loaded("#email",1000)
+    email_input=driver.find_element_by_css_selector("#email")
+    pass_input=driver.find_element_by_css_selector("#pass")
+    login_button=driver.find_element_by_css_selector("#u_0_b")
+    email_input.send_keys(username)
+    pass_input.send_keys(password)
+    time.sleep(2)
+    login_button.click()
 
-check_Loaded("#email",1000)
-email_input=driver.find_element_by_css_selector("#email")
-pass_input=driver.find_element_by_css_selector("#pass")
-login_button=driver.find_element_by_css_selector("#u_0_b")
-email_input.send_keys(username)
-pass_input.send_keys(password)
-time.sleep(2)
-login_button.click()
+login()
+
+   
 time.sleep(3)
 profile=driver.find_element_by_css_selector("._1k67._cy7")
 profile.click()
 
-driver.get("https://www.facebook.com/alfonso.garciaminaur/photos?lst=838059340%3A838059340%3A1594990536")
+#driver.get("https://www.facebook.com/alfonso.garciaminaur/photos?lst=838059340%3A838059340%3A1594990536")
+time.sleep(2)
+photos_link=driver.find_elements_by_css_selector("._6-6")
+photos_link[3].click()
 time.sleep(3)
 driver.execute_script(f"window.scrollTo(0, 1000)") 
 time.sleep(3)
@@ -72,7 +79,7 @@ index=0
 errors=0
 for pic in pictures:
     try:
-        print(pic["href"])
+
         driver.get(pic["href"])
         time.sleep(0.5)
         check_Loaded(".spotlight",1000)
